6657d0f314830ba025d4e0e7625375866fb69012
Ammarnet <115216900+Amma7@users.noreply.github.com>
2026-05-24 22:35:28 +0300

    Delete bootstrap-5.0.2-dist directory

--- bootstrap-5.0.2-dist/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-     $(".navbar-toggler").blur(function(event){
-        var screenWidth = Window.innerWidth;
-        if(screenWidth < 768){
-            $("#navbarSupportedContent").collapse('hide');
-        }
-     });
-});
